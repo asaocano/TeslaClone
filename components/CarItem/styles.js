16f69663ef94332a72c2302d57118ab4ffb31c78
asaocano <manuelocano9@gmail.com>
2022-01-13 14:23:24 -0700

    Header component

--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -1,11 +1,11 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 const styles = StyleSheet.create({
   carContainer: {
     width: "100%",
-    height: "100%",
+    height: Dimensions.get("window").height,
   },
   titles: {
-    marginTop: "20%",
+    marginTop: "30%",
     width: "100%",
     alignItems: "center",
   },
---
+++ components/CarsList/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, FlatList, Dimensions } from "react-native";
+import cars from "../CarsList/cars";
+import CarItem from "../CarItem";
+import styles from "./styles";
+
+const CarsList = (props) => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={cars}
+        renderItem={({ item }) => <CarItem car={item} />}
+        showsVerticalScrollIndicator={false}
+        snapToAlignment={"start"}
+        decelerationRate={"fast"}
+        snapToInterval={Dimensions.get("window").height}
+      />
+    </View>
+  );
+};
+
+export default CarsList;
