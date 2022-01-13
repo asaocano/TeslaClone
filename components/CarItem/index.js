bca473b494c6334ee8b6225eb69d1f5d337d1d5a
asaocano <manuelocano9@gmail.com>
2022-01-13 13:06:46 -0700

    Componentes para botones

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, ImageBackground } from "react-native";
+import StyledButton from "../Buttons";
 import styles from "./styles";
 
 const CarItem = (props) => {
@@ -13,6 +14,22 @@ const CarItem = (props) => {
         <Text style={styles.title}>Model S</Text>
         <Text style={styles.subtitle}>Starting at $69,420</Text>
       </View>
+
+      <StyledButton
+        type="primary"
+        content="Custom order"
+        onPress={() => {
+          console.warn("Custom order was pressed!");
+        }}
+      />
+
+      <StyledButton
+        type="secondary"
+        content="Existing inventory"
+        onPress={() => {
+          console.warn("Existing inventory was pressed!");
+        }}
+      />
     </View>
   );
 };
